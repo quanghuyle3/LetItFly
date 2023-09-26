23dfe992999beb106fe6156116d0b33defd866bb
Quang Le <quanghuyle3@gmail.com>
2023-09-25 23:34:34 -0700

    Initialize Spring Boot Application

---
+++ src/main/java/com/project/LetItFly/LetItFlyApplication.java
@@ -0,0 +1,13 @@
+package com.project.LetItFly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetItFlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetItFlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/LetItFly/LetItFlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.LetItFly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetItFlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
