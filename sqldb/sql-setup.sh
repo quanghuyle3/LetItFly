754da9bf9083b3712c4b5844f0f7e7ba78e8ed8f
gokris165 <65064454+gokris165@users.noreply.github.com>
2023-09-30 14:23:10 -0700

    Updated docker server port
    docker port 3306 mapped to local port 4000

--- sqldb/sql-setup.sh
+++ sqldb/sql-setup.sh
@@ -6,4 +6,4 @@ docker build -t mysql-server-img .
 # command to create a docker container named "lif-sql-server"
 # with exposing docker port 3306 to localhost port 3306
 # from image "mysql-server-img"
-docker create --name lif-sql-server -p 3306:3306 mysql-server-img
+docker create --name lif-sql-server -p 4000:3306 mysql-server-img
