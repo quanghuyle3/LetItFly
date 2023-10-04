45b06eb1ef17ede5e361f8519b079a8597741de7
Joshua Olszewski <you@example.com>
2023-10-03 18:24:11 -0700

    added models for DriverStatus, HistoryLog, Payment, and UserRole. Need help with cascade.

---
+++ src/main/java/com/project/LetItFly/model/Payment.java
@@ -0,0 +1,123 @@
+package com.project.LetItFly.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "payment")
+public class Payment
+{
+    @Id
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    @Column(name = "expiration")
+    private String expiration;
+
+    @Column(name = "cvv")
+    private int cvv;
+
+    @Column(name = "type")
+    private String type;
+
+    @ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH })
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "billing_address")
+    private String billingAddress;
+
+    @Column(name = "balance")
+    private double balance;
+    
+    public Payment() {
+    }
+
+    public Payment(String expiration, int cvv, String type, User user, String name, String billingAddress, double balance) {
+        this.expiration = expiration;
+        this.cvv = cvv;
+        this.type = type;
+        this.user = user;
+        this.name = name;
+        this.billingAddress = billingAddress;
+        this.balance = balance;
+    }
+
+
+    public String getExpiration() {
+        return this.expiration;
+    }
+
+    public void setExpiration(String expiration) {
+        this.expiration = expiration;
+    }
+
+    public int getCvv() {
+        return this.cvv;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBillingAddress() {
+        return this.billingAddress;
+    }
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " expiration='" + getExpiration() + "'" +
+            ", cvv='" + getCvv() + "'" +
+            ", type='" + getType() + "'" +
+            ", user='" + getUser() + "'" +
+            ", name='" + getName() + "'" +
+            ", billingAddress='" + getBillingAddress() + "'" +
+            ", balance='" + getBalance() + "'" +
+            "}";
+    } 
+}
---
+++ src/main/java/com/project/LetItFly/model/UserRole.java
@@ -0,0 +1,53 @@
+package com.project.LetItFly.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_role")
+public class UserRole {
+
+    @Id
+    //not sure what cascade to put here
+    @JoinColumn(name = "user_id")
+    private int userId;
+
+    @Id
+    //not sure what cascade to put here
+    @JoinColumn(name = "role_id")
+    private int roleId;
+
+    public UserRole() {
+    }
+
+    public UserRole(int userId, int roleId) {
+        this.userId = userId;
+        this.roleId = roleId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole [userId=" + userId + ", roleId=" + roleId + "]";
+    }  
+}
