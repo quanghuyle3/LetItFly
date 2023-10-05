587bcc4528429a03c235b6679f362eb88eedc028
Quang Le <quanghuyle3@gmail.com>
2023-10-05 16:15:30 -0700

    Update User and Role models

--- src/main/java/com/project/LetItFly/model/Role.java
+++ src/main/java/com/project/LetItFly/model/Role.java
@@ -1,10 +1,17 @@
 package com.project.LetItFly.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
