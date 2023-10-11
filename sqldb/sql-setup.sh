e4671347d93012df001fc219980ba1c17c1121d7
gokris165 <gokris165@gmail.com>
2023-10-11 13:29:41 -0700

    Sql: modified sql-setup bash file

--- sqldb/sql-setup.sh
+++ sqldb/sql-setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# stop docker container if running
+docker stop lif-sql-server
+
+# remove docker container if it exists
+docker rm lif-sql-server
+
+# remove docker image if it exists
+docker rmi mysql-server-img
+
 # docker command to build the image from a Dockerfile
 docker build -t mysql-server-img .
 
 # command to create a docker container named "lif-sql-server"
-# with exposing docker port 3306 to localhost port 3306
+# with exposing docker port 3306 to localhost port 4000
 # from image "mysql-server-img"
 docker create --name lif-sql-server -p 4000:3306 mysql-server-img
