2deaab9b809cddb26b02c6d3f0499b4f166e957f
gokris165 <gokris165@gmail.com>
2023-10-11 17:10:38 -0700

    Frontend: added basic map and changed file structure

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Header from "../components/Header";
+import Map from "../components/Map";
+import History from "../components/History";
 // import styled from "styled-components";
 
 function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Map />
+      <History />
+    </>
+  );
 }
 
 export default Home;
