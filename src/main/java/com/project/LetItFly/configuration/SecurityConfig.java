64d4e6fd62bf89d8f56b01be498bc8c05b3040aa
Quang Le <quanghuyle3@gmail.com>
2023-10-14 13:59:36 -0700

    Resolved CORS

--- src/main/java/com/project/LetItFly/configuration/SecurityConfig.java
+++ src/main/java/com/project/LetItFly/configuration/SecurityConfig.java
@@ -1,17 +1,31 @@
 package com.project.LetItFly.configuration;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.project.LetItFly.service.UserService;
 
 @Configuration
+@EnableWebMvc
 public class SecurityConfig {
+
+    private static final Long MAX_AGE = 3600L;
+    private static final int CORS_FILTER_ORDER = -102;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -41,5 +55,30 @@ public class SecurityConfig {
         http.httpBasic(Customizer.withDefaults());
         http.csrf((csrf) -> csrf.disable());
         return http.build();
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.AUTHORIZATION,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT));
+        config.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()));
+        config.setMaxAge(MAX_AGE);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+
+        // should be set order to -100 because we need to CorsFilter before
+        // SpringSecurityFilter
+        bean.setOrder(CORS_FILTER_ORDER);
+        return bean;
     }
 }
