db08cea44fdcff7a42b3af34ecfcf2b509f793d5
Quang Le <quanghuyle3@gmail.com>
2023-10-17 13:00:46 -0700

    Updated security with JWT token

--- src/main/java/com/project/LetItFly/configuration/SecurityConfig.java
+++ src/main/java/com/project/LetItFly/configuration/SecurityConfig.java
@@ -7,49 +7,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import com.project.LetItFly.service.UserService;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebMvc
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private static final Long MAX_AGE = 3600L;
     private static final int CORS_FILTER_ORDER = -102;
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider(UserService userService) {
-        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService(userService); // set the custom user details service
-        auth.setPasswordEncoder(passwordEncoder());
-        return auth;
-    }
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer -> configurer
                 .requestMatchers("/login").permitAll()
                 .requestMatchers("/registration").permitAll()
-                .anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginProcessingUrl("/authenticateTheUser")
-                        .failureHandler(null)
-                        .permitAll())
+                // .anyRequest().authenticated())
+                .anyRequest().permitAll())
+                .sessionManagement((sessionManagement) -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout(logout -> logout.permitAll());
 
         http.httpBasic(Customizer.withDefaults());
