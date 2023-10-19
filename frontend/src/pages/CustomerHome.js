7a034d37c0f48aab93afa3f609c651d5be7fcf16
gokris165 <65064454+gokris165@users.noreply.github.com>
2023-10-18 17:35:41 -0700

    Vanilla map: Added map routes, bug: overlays over previous routes, directions don't use lat-lng (#13)
    * following google's documentation, issue: map renders per user input
    * Autocomplete succesfully implemented
    * Map: added routes, bug: old routes not removed, directions api not using lat-lng
    * updated package-lock.json file after installing mui

--- frontend/src/pages/CustomerHome.js
+++ frontend/src/pages/CustomerHome.js
@@ -1,13 +1,47 @@
 import Header from "../components/Header";
 import Map from "../components/Map";
 import History from "../components/History";
-// import styled from "styled-components";
+import SearchBar from "../components/SearchBar";
+import { Loader } from "@googlemaps/js-api-loader";
+import { useEffect, useRef } from "react";
+
+/**
+ * ISSUES:
+ *  - remove old routes before rendering new routes
+ *  - find a way to store current location, async pain
+ *  - make the directions api use lat/lng instead of strings
+ *
+ */
 
 function CustomerHome() {
+  const currentMap = useRef();
+  const userLocation = useRef();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        userLocation.current = { lat: latitude, lng: longitude };
+      }
+    );
+  }, []);
+
+  // Initialize google map api loader
+  const loader = new Loader({
+    apiKey: process.env.REACT_APP_GOOGLE_KEY,
+  });
   return (
     <>
       <Header />
-      <Map />
+      <SearchBar
+        Loader={loader}
+        currentMap={currentMap}
+        userLocation={userLocation}
+      />
+      <Map
+        Loader={loader}
+        currentMap={currentMap}
+        userLocation={userLocation}
+      />
       <History />
     </>
   );
