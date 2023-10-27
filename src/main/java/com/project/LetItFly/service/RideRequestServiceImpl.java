eadf05e0755bf9eb08db790d68a280499f4bc912
Quang Le <65510865+quanghuyle3@users.noreply.github.com>
2023-10-26 23:07:24 -0700

    Backend dev (#20)
    * Add roleName to AuthenticationResponse
    
    * Added RideRequest entity and APIs
    
    * Incorporated JWT, protected REST APIs
    
    * Updated database for Ride Request
    
    * Updated Driver Status model
    
    * Adjusted APIs for Ride Request

--- src/main/java/com/project/LetItFly/service/RideRequestServiceImpl.java
+++ src/main/java/com/project/LetItFly/service/RideRequestServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.project.LetItFly.model.RideRequest;
+import com.project.LetItFly.model.User;
 import com.project.LetItFly.repository.RideRequestRepository;
+import com.project.LetItFly.repository.UserRepository;
 import com.project.LetItFly.requestModel.RideRequestRequest;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class RideRequestServiceImpl implements RideRequestService {
 
     private final RideRequestRepository rideRequestRepository;
+    private final UserRepository userRepository;
 
     @Override
     public RideRequest findRideRequestById(int id) {
@@ -27,17 +30,43 @@ public class RideRequestServiceImpl implements RideRequestService {
     }
 
     @Override
-    public String save(RideRequestRequest rideRequestRequest) {
+    public int save(RideRequestRequest rideRequestRequest) {
         RideRequest rideRequest = new RideRequest(rideRequestRequest);
-        rideRequestRepository.save(rideRequest);
 
-        return "SUCCESS";
+        // find passenger object from passenger id
+        User user = userRepository.findUserById(rideRequestRequest.getPassengerId());
+
+        // associate passenger object to the ride request
+        rideRequest.setPassengerId(user);
+
+        try {
+            RideRequest rq = rideRequestRepository.save(rideRequest);
+            return rq.getId();
+        } catch (Exception e) {
+            return 0;
+        }
     }
 
     @Override
     public String delete(int id) {
         rideRequestRepository.deleteById(id);
         return "SUCCESS";
+    }
+
+    // @Override
+    public RideRequest findRideRequestByPassengerId(int passengerId) {
+        // retrieve the passenger object first
+        User passenger = userRepository.findUserById(passengerId);
+
+        return rideRequestRepository.findRideRequestByPassengerId(passenger);
+    }
+
+    @Override
+    public List<RideRequest> findRideRequestByDriverId(int driverId) {
+        // retrieve the passenger object first
+        User driver = userRepository.findUserById(driverId);
+
+        return rideRequestRepository.findRideRequestByDriverId(driver);
     }
 
 }
