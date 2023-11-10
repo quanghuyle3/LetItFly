2a2f9df39d1d2644da708c5353bdc1e941640868
gokris165 <gokris165@gmail.com>
2023-11-09 19:51:30 -0800

    Driver ride: added todo comments

--- frontend/src/pages/DriverRide.js
+++ frontend/src/pages/DriverRide.js
@@ -1,5 +1,16 @@
 import { useLocation } from "react-router-dom";
 
+/**
+ * TODO:
+ * Phase 1:
+ *  - render map with directions from current location to passenger
+ *  - update current location and passenger location in real time
+ *  - check distance, if distance < 50 meters ? move to phase 2
+ * Phase 2:
+ *  - render map with directions from current location to destination
+ *  - update current location on map in real time
+ *  - check distance, if distance < 50 meters ? display ride success page
+ */
 function Home() {
   const {
     state: { cookie, rideRequest },
